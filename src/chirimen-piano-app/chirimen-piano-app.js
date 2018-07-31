da06be0fcab4ddc6c823d958e293bfe478353ca5
sizuhiko <k-kishida@esm.co.jp>
2018-07-31 22:06:01 +0900

    WebI2C以外の部分の実装

---
+++ src/chirimen-piano-app/chirimen-piano-app.js
@@ -0,0 +1,113 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+import '@polymer/polymer/lib/elements/dom-repeat.js';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
+import '@polymer/paper-ripple/paper-ripple.js';
+import '@polymer/paper-slider/paper-slider.js';
+import '@polymer/iron-icons/av-icons.js';
+import '@polymer/iron-icon/iron-icon.js';
+import './cp-player.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class ChirimenPianoApp extends GestureEventListeners(PolymerElement) {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        .key {
+          position: relative;
+          display: inline-block;
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+          overflow: hidden;
+          transition: box-shadow 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+          transition-delay: 0.2s;
+          box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+          margin: 10px;
+        }
+        .center {
+          margin-top: 15px;
+          text-align: center;
+        }
+        .control {
+          background-color: #4285f4;
+        }
+        .control > iron-icon {
+          margin: 16px;
+        }
+        .control > iron-icon {
+          --iron-icon-fill-color: #fff;
+        }
+        paper-slider {
+          width: 500px;
+        }
+      </style>
+
+      <div class="control key raised" on-tap="prev">
+        <iron-icon icon="av:fast-rewind"></iron-icon>
+        <paper-ripple class="circle" recenters></paper-ripple>
+      </div>
+
+      <template is="dom-repeat" items="[[_fromPage(page)]]">
+        <div class="key raised" on-tap="play">
+          <div class="center">[[item.name]]</div>
+          <paper-ripple class="circle" recenters></paper-ripple>
+        </div>
+      </template>
+
+      <div class="control key raised" on-tap="next">
+        <iron-icon icon="av:fast-forward"></iron-icon>
+        <paper-ripple class="circle" recenters></paper-ripple>
+      </div>
+
+      <div>
+        <paper-slider value="{{volume}}"></paper-slider>
+      </div>
+      <cp-player id="player" hz="[[hz]]" volume="[[volume]]"></cp-player>
+    `;
+  }
+  static get properties() {
+    return {
+      hz: Number,
+      volume: Number,
+      page: Number,
+    };
+  }
+  // page の範囲は -2 < page < 3 で、3オクターブまで
+  _fromPage(page) {
+    // key 0 は標準のド
+    var hz = function(key) { return 442 * Math.pow(2, (1 / 12) * (key - 9)); };
+    var keys = [
+      [{name: 'ド', hz: hz(-12)}, {name: 'レ', hz: hz(-10)}, {name: 'ミ', hz: hz(-8)}, {name: 'ファ', hz: hz(-7)}],
+      [{name: 'ソ', hz: hz(-5)}, {name: 'ラ', hz: hz(-3)}, {name: 'シ', hz: hz(-1)}, {name: 'ド', hz: hz(0)}],
+      [{name: 'ド', hz: hz(0)}, {name: 'レ', hz: hz(2)}, {name: 'ミ', hz: hz(4)}, {name: 'ファ', hz: hz(5)}],
+      [{name: 'ソ', hz: hz(7)}, {name: 'ラ', hz: hz(9)}, {name: 'シ', hz: hz(11)}, {name: 'ド', hz: hz(12)}],
+      [{name: 'ド', hz: hz(12)}, {name: 'レ', hz: hz(14)}, {name: 'ミ', hz: hz(16)}, {name: 'ファ', hz: hz(17)}],
+      [{name: 'ソ', hz: hz(19)}, {name: 'ラ', hz: hz(21)}, {name: 'シ', hz: hz(23)}, {name: 'ド', hz: hz(24)}],
+    ];
+    return keys[page + 2];
+  }
+  play(event) {
+    this.hz = event.model.item.hz;
+    this.$.player.play();
+  }
+  prev() {
+    if (this.page > -2) this.page--;
+  }
+  next() {
+    if (this.page < 3) this.page++;
+  }
+  ready() {
+    super.ready();
+    this.page = 0;
+    this.volume = 50;
+  }
+}
+
+window.customElements.define('chirimen-piano-app', ChirimenPianoApp);
---
+++ src/chirimen-piano-app/cp-player.js
@@ -0,0 +1,36 @@
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class CpPlayer extends PolymerElement {
+  static get properties() {
+    return {
+      hz: Number,
+      volume: Number,
+    };
+  }
+  play() {
+    var osciillator = this._audioCtx.createOscillator();
+    osciillator.frequency.value = this.hz;
+    var gainNode = this._audioCtx.createGain();
+    osciillator.connect(gainNode);
+    var audioDestination = this._audioCtx.destination;
+    gainNode.connect(audioDestination);
+    gainNode.gain.value = this.volume / 100;
+
+    osciillator.start = osciillator.start || osciillator.noteOn;
+    osciillator.start();
+    setTimeout(function() {
+        osciillator.stop();
+    }, 500);
+  }
+  ready() {
+    super.ready();
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    this._audioCtx = new AudioContext();
+  }
+}
+
+window.customElements.define('cp-player', CpPlayer);
